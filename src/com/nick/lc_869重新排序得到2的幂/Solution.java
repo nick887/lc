143cdda4e722920a0911beb34e0225f7e0f221b4
nick <2975684744@qq.com>
2021-10-28 16:44:08 +0800

    date:10/29

---
+++ src/com/nick/lc_869重新排序得到2的幂/Solution.java
@@ -0,0 +1,10 @@
+package com.nick.lc_869重新排序得到2的幂;
+
+import java.util.HashMap;
+
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        return false;       
+    }
+}
